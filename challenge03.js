565ab40fc856e6d91ea7e22a1ad15e94c3270d10
Hademinne <hademinne@gmail.com>
2024-06-02 15:08:03 +0000

    challenge 3

---
+++ challenge03.js
@@ -0,0 +1,4 @@
+//challenge 03
+//sum all the numbers of a given array(cq.list). except the highest and the lowest element(by value not by index) The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+//exp: {6,2,1,8,10} =>16  or {1,1,11,2,3} =>6
