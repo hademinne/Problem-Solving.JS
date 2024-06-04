89e7bb84145711956d383e9c78909e9dc7e07c2c
Hademinne <hademinne@gmail.com>
2024-06-04 17:07:16 +0000

    challenge 07

---
+++ challenge07.js
@@ -0,0 +1,9 @@
+//challenge 07
+//Opposite number: very simple given an integer or floating point number, find its opposite
+
+//ex: 1: -1  ,  14: -14 , -20: 20
+
+function oppositeNum(num) {
+  return num > 0 ? -num : -num;
+}
+console.log(oppositeNum(-14));
