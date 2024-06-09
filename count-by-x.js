d27c093cac85b4783847b6534f76a17f27643dc3
Hademinne <hademinne@gmail.com>
2024-06-09 13:31:09 +0000

    count by x

---
+++ count-by-x.js
@@ -0,0 +1,19 @@
+// count by x
+// create a function with two arguments that will return an array of the first n multiply of x
+//assume both the given number and numbers of times to count will be positive numbers greater than 0.
+//return the results as an array.
+
+//ex: (1,10) => [1,2,3,4,5,6,7,8,10]  (2,5)=>[2,4,6,8,10]
+
+function countBy(x, n) {
+  let z = [];
+  let count = 0;
+  // z.push(x, n);
+  for (let i = 1; i <= n; i++) {
+    z.push(x * i);
+  }
+
+  return z;
+}
+
+console.log(countBy(1, 10));
