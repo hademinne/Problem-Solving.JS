48a6bd5c56e1cecf37ef135fe55ad2c3997614e4
Hademinne <hademinne@gmail.com>
2024-06-11 16:34:23 +0000

    convert number to a string

---
+++ convert-to-string.js
@@ -0,0 +1,10 @@
+/*
+convert number to a string :
+we need a function can transform a number to a string .
+*/
+function numberToString(num) {
+  // return num.toString();
+  return `${num}`;
+}
+
+console.log(typeof numberToString(123));
