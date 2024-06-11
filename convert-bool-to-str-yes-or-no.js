4bd7767d0ad2eef5dae9e607d9bca07699807139
Hademinne <hademinne@gmail.com>
2024-06-11 15:10:18 +0000

    convert boolean value to strings

---
+++ convert-bool-to-str-yes-or-no.js
@@ -0,0 +1,11 @@
+/*
+convert boolean value to strings 'yes' or 'no'
+complete the method that takes a boolean value and return a "yes " string for true or a 'no' string for false
+
+*/
+
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
+
+console.log(boolToWord(true));
