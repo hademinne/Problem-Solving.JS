69dbec9bb48cc82cde64d3204ae52428c7fac431
Hademinne <hademinne@gmail.com>
2024-06-16 03:39:48 +0000

    exclusive "or" logical operator

---
+++ exclusive-or.js
@@ -0,0 +1,8 @@
+/*
+exclusive "or" logical operator:
+the exclusive or evaluates two booleans. it then returns true if exactly one of the two expressions are true. false otherwise.
+*/
+function xor(a, b) {
+  return a === b ? false : true;
+}
+console.log(xor());
