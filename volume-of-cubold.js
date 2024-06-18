e9ad1e6b717b10e8d048058c4c321eecc4324727
Hademinne <hademinne@gmail.com>
2024-06-18 15:18:05 +0000

    volume of a cubold

---
+++ volume-of-cubold.js
@@ -0,0 +1,10 @@
+/*
+volume of a cubold : bob needs a fast way to calculate the volume of cuboid with three values. length , width and  the height ot the cuboid. write a func to  help bob with caculation
+*/
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+console.log(kata.getVolumeOfCuboid(5, 10, 5));
