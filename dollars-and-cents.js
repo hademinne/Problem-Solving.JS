e84b16ab1f7e689ac8c9ed4cb3aa653159ac89a5
Hademinne <hademinne@gmail.com>
2024-06-20 18:58:41 +0000

    Dollars and Cents

---
+++ dollars-and-cents.js
@@ -0,0 +1,12 @@
+/*
+Dollars and Cents:
+the company you work for has just been awarded a contract to build a payment gateway. in order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+ex: 39.99. becomes $39.99
+*/
+
+function formatMoney(amount) {
+  return `$${amount.toFixed(2)}`;
+}
+
+console.log(formatMoney(39.99));
